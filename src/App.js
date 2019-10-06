5159c8fc1cf0f4b6c3c0ed159108e4fdee1d0340
Manav <ifightwars@gmail.com>
2019-10-05 19:40:22 -0700

    basic frontend

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const head = require('./img/loan_app_status.png');
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={head} className="App-logo" alt="head"/>
       </header>
+
+      <div className="Body">
+        <h1>test  </h1>
+      </div>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,5 @@
+const images = [
+	{id: 1, src : './img/loan_app_status.png'},
+]
+
+export default images;
