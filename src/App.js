bf117f1e624d3cf8781486202a7e0f8dadfb88dd
Manav <ifightwars@gmail.com>
2019-10-09 20:10:27 -0700

    Some HTML changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
 
-const head = require('./img/loan_app_status.png');
-const dev_token = 'XiCoH70Q2tJ6Z48EVhDXnAiuqH6dzL9U'
+const head = require('./img/loan_app_status.png')
+const cont_button = require('./img/continue.png')
+const dev_token = 'bTb5J6BimTcx6AO6ideFb8dlXy0FjiCm'
 
  function boxUpload () {
    //Append the file and folder ID to the FormData object to pass as the body parameter to Box
 
    let form = new FormData();
-   form.append('name', document.getElementById('myFile').files[0])
-   form.append('id', '89298802436')
+   form.append('file', document.getElementById('myFile').files[0])
+   form.append('parent_id', '89298802436')
 
-   //Using fetch API to make a POST request to Box's file upload endpoint
-   //Passes the developer token through an authorization header
+   // Using fetch API to make a POST request to Box's file upload endpoint
+   // Passes the developer token through an authorization header
 
    fetch('https://upload.box.com/api/2.0/files/content', {
     method: 'POST',
@@ -23,7 +22,8 @@ const dev_token = 'XiCoH70Q2tJ6Z48EVhDXnAiuqH6dzL9U'
     }),
     body: form
    }).then((response) => {
-    console.log(response);
+    document.getElementById("thanks").style.display="block"
+    document.getElementById("continue").style.display="block"
    })
  }
 
@@ -36,11 +36,12 @@ function App() {
 
       <div className="Body">
         <h1>Your Loan Request</h1>
-
         <h3>To give you the most accurate loan information, please upload your application form here.</h3>
-
-        <label for="userUpload">Upload your loan application here.</label>
-        <input type="file" id="myFile" onChange={() => boxUpload()}/>
+        <div className="uploadForm">  
+          <input type="file" id="myFile" onChange={() => boxUpload()}/>
+          <h3 id="thanks">Thank you! Your application is being securely stored with Box!</h3>
+          <img id="continue" a="_blank" src={cont_button}/>
+        </div>
       </div>
     </div>
   );
